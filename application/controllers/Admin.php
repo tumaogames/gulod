3c9e1673aeb4dccd40c659156fcffefa335da81f
Art Zolina III <artchel06studios@yahoo.com>
2023-08-05 17:01:29 +0800

    edit route5

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,24 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Admin extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('admin');
-	}
+    public function index()
+    {
+        $this->load->view('admin');
+    }
+
+    // Add other methods for handling admin panel functionality as needed.
 }
+
+$route['default_controller'] = 'welcome';
+$route['gulod/admin'] = 'admin';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
