3e48a1966c6ddb4fa593ac5a6926d51cb9bca847
Art Zolina III <artchel06studios@yahoo.com>
2023-08-05 16:53:28 +0800

    edit route3

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 error_reporting(E_ALL);
-class Welcome extends CI_Controller {
+class Admin extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
