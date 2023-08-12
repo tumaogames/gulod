fc09e67cc57ea003daa40cf7a702530ae0690757
Art Zolina III <artchel06studios@yahoo.com>
2023-08-12 21:00:07 +0800

    fix url

--- app/Views/print.php
+++ app/Views/print.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="http://localhost:8080/assets/mystyle.css">
+    <link rel="stylesheet" type="text/css" href="https://unligames.com/gulod/assets/mystyle.css">
     <style>
         /* Override Bootstrap's bg-primary for column */
         .bg-primary {
