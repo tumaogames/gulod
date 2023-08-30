8c021c4a250d91d868b62bc409cb0c4d3edcd13e
Art Zolina III <artchel06studios@yahoo.com>
2023-08-30 14:55:26 +0800

    edit registration

--- app/Views/registration_view.php
+++ app/Views/registration_view.php
@@ -24,33 +24,33 @@
         <div class="row justify-content-center p-5">
             <div class="col-md-4">
                 <h2 class="text-center mb-4">Admin Panel Registration</h2>
-                <form id="registrationForm" action="/register_user" method="post">
+                <form id="registrationForm" action="<?= base_url() ?>/register_user" method="post">
                     <?= csrf_field() ?>
                     <div class="form-group">
                         <label for="username">Username</label>
-                        <input type="text" class="form-control" id="username" name="username" required>
+                        <input type="text" class="form-control" id="username" name="username" required value="<?php old('username') ?>">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" required>
+                        <input type="password" class="form-control" id="password" name="password" required value="<?php old('password') ?>">
                     </div>
                     <div class="form-group">
                         <label for="confirm_password">Confirm Password</label>
-                        <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
+                        <input type="password" class="form-control" id="confirm_password" name="confirm_password" required value="<?php old('confirm_password') ?>">
                     </div>
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" required>
+                        <input type="email" class="form-control" id="email" name="email" required value="<?php old('email') ?>">
                     </div>
                     <div class="form-group">
                         <label for="full_name">Full Name</label>
-                        <input type="text" class="form-control" id="full_name" name="full_name" required>
+                        <input type="text" class="form-control" id="full_name" name="full_name" required value="<?php old('full_name') ?>">
                     </div>
                     <div class="form-group">
                         <label for="date_of_birth">Date of Birth</label>
-                        <input type="date" class="form-control" id="date_of_birth" name="date_of_birth" required>
+                        <input type="date" class="form-control" id="date_of_birth" name="date_of_birth" required value="<?php old('date_of_birth') ?>">
                     </div>
-                    <div><a href="/admin_login">Login</a></div>
+                    <div><a href="<?= base_url() ?>/login_user">Login</a></div>
                     <button type="submit" class="btn btn-primary btn-block">Register</button>
                 </form>
             </div>
